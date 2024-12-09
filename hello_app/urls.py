783007a02e694c4eb0354d51ec4d7366757f8004
LukaszDomaracki <domaracki@agh.edu.pl>
2024-12-09 12:40:20 +0100

    views and templates

--- hello_app/urls.py
+++ hello_app/urls.py
@@ -5,4 +5,10 @@ from hello_app import views
 
 urlpatterns = [
     path("", views.hello),
+    path(
+        "param/<str:name>",
+        views.hello_name,  # param, bo inaczej chwytałoby wszystkie stringi,
+    ),  # przykład parametryzacji. W konwenterzze nazwa argumentu z funckji (muszą być te same nazwy)
+    # może być samo <name> i też zadziała, jako domyslny
+    path("template/<str:name>", views.hello_template),
 ]
--- hello_app/views.py
+++ hello_app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.utils.html import escape
 
 
 def hello(request):  # request to tylko konwencja (może być nawet 'dupa')
     return HttpResponse(
         "Hello World!"
     )  # Widoki nie mogą zwracać byle czego – mają zwracać obekt klasy HttpResponse
+
+
+def hello_name(request, name):  # w urlu i tutaj musi być ta sama nazwa parametru
+    name = escape(
+        name
+    )  # zmienia name tak by np javascript zmienił się w input i specjalne znaki zmienia na ich dpowiedniki
+    return HttpResponse(f"Hello {name}!")
+
+
+def hello_template(request, name):
+    return render(
+        request, "hello.html", context={"name": name}
+    )  # tutaj zamiast "name" może być "x" ale w tedy w szablonie odwołujemy się do "x"
