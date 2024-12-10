80cdb8e7e6e4735b44ceec6550a4b9f75967df81
LukaszDomaracki <domaracki@agh.edu.pl>
2024-12-10 12:01:50 +0100

    form list

--- form_app/views.py
+++ form_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
 TASKS = []
 
@@ -9,3 +10,21 @@ def form1(request):
     if task:
         TASKS.append(task)
     return render(request, "form_app/form1.html")
+
+
+def form2(request):
+    if request.method == "GET":
+        return render(request, "form_app/form2.html")
+    elif request.method == "POST":
+        data = request.POST
+        task = data.get("task")
+        if task:
+            TASKS.append(task)
+            return redirect("form_app:form2")
+        return render(request, "form_app/form2.html")
+    else:
+        return HttpResponse(status=405)
+
+
+def task_list(request):
+    return render(request, "form_app/list.html", context={"tasks": TASKS})
